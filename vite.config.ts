17dedd244e26312fec131eb2657a591336ae68be
Droliverzh <dr.oliverzhao@gmail.com>
2025-09-15 14:27:25 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
     plugins: [react()],
-    base: mode === 'production' ? '/ming-dance-app/' : '/',
+    base: mode === 'production' ? '/mingdance.github.io/' : '/',
 }))
